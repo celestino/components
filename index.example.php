7d8262f0d3ea2d41362d18129666e53feb4e725f
Celestino Diaz <celestino.diaz@gmx.de>
2012-04-23 03:30:56 +0300

    updated application example

--- index.example.php
+++ index.example.php
@@ -22,34 +22,36 @@
      * The (vendor or own) modules can be located all in one directory or different ones, its up to you.
      * This example namespaces registrations are used in the example modules below.
      * Register the autoloader instance as callback to php.
+     * @see \Brickoo\Core\Autoloader
      */
     $Autoloader = new Autoloader();
     $Autoloader->registerNamespace('Brickoo', '/path/to/directory/containing_brickoo')
-               ->registerNamespace('YourCompany/Forum', '/path/to/directory/containing_your_module/')
-               ->registerNamespace('OtherCompany/Blog', '/path/to/directory/containg_vendor_modules')
+               ->registerNamespace('YourCompany', '/path/to/modules/company')
+               ->registerNamespace('OtherCompany', '/path/to/modules/vendor')
                ->register();
 
     /**
      * Create an instance of the Application class.
-     * The Application class holds anRegistry instance so you can register any global configuration needed.
+     * The Application class holds a Registry instance so you can register any global configuration needed.
      * Values will be copy by value and made readonly while objects are not cloned.
-     * Registers the Autoloader to the Registry to offer modules the ability to register further registrations.
-     * Set the public directory path where all direct accessed file (css, js, images) are located (subdirectories).
-     * Register vendors and own modules available, this makes it possible to select which modules
-     * should be explicit available and can be accessed through the router.
+     * Registers the Autoloader to the Registry offering modules the ability to register further namespaces.
+     * Set the public directory path where all direct accessed files (css, js, images) are located (subdirectories).
+     * Register vendors and own modules, this makes it possible to select which modules
+     * should be explicit available and should be accessed through the router.
      * The modules registrations are also used to look for routes, infos and any other information
      * which should be located in the root path of each module.
      *
      * The last step is to run the application which does call all events to setup and run your application.
      * We do use the example http application, but you can build of course your own application
      * containing the logic you are conform with.
+     * @see \Brickoo\Core\Application
      * @see \Brickoo\Http\Application
      */
     $Application = new Application();
     $Application->registerAutoloader($Autoloader)
                 ->registerPublicDirectory('/public')
                 ->registerModules(array(
-                    'YourCompany/Forum'    => '/path/to/modules/YourCompany/Forum',
+                    'YourCompany/Forum'    => '/path/to/modules/company/YourCompany/Forum',
                     'OtherCompany/Blog'    => '/path/to/modules/vendor/OtherCompany/Blog'
                 ))
                 ->run(new MainApplication());
