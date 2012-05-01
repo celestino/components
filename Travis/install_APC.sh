c4db0b5983aed392adbaad14b261a7b00f56e5ff
Celestino Diaz <celestino.diaz@gmx.de>
2012-05-01 19:36:15 +0300

    fixed before script for the APC extension

--- Travis/install_APC.sh
+++ Travis/install_APC.sh
@@ -2,8 +2,12 @@
 wget "http://pecl.php.net/get/APC"
 tar -xzf APC
 cd APC-*
-phpize && ./configure && make && make install
+phpize
+./configure
+sudo make install
+INI_DIR=`php --ini | grep "Scan for additional" | sed -e "s|.*:\s*||"`
+INI_FILE="$INI_DIR/apc.ini"
 echo "extension=apc.so
 [apc]
 apc.enabled = 1
-apc.enable_cli = 1" > `php --ini | grep "Loaded Configuration File" | sed -e "s|.*:\s*||"`
+apc.enable_cli = 1" > $INI_FILE
