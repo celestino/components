58755f805ad5af87ccb02695bfb4259437b0c107
Celestino Diaz <celestino.diaz@gmx.de>
2012-05-01 19:54:57 +0300

    modified travis APC install script

--- Tests/travis_install_APC.sh
+++ Tests/travis_install_APC.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-wget http://pecl.php.net/get/APC
-tar -xzf APC-*
-sh -c "cd APC-* && phpize && ./configure --enable-apc && make && sudo make install"
+echo "Downloading APC-3.1.10 ..."
+wget http://pecl.php.net/get/APC-3.1.10.tgz
+tar -xzf APC-3.1.10.tgz
+sh -c "cd APC-3.1.10 && phpize && ./configure --enable-apc && make && sudo make install"
 echo "extension=apc.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+echo "Done!" 
