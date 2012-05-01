0400b7455a1e8ecceb09416a3ea926724c9b33ef
Celestino Diaz <celestino.diaz@gmx.de>
2012-05-01 20:23:01 +0300

    moved APC install to travis.yml

--- Tests/travis_install_APC.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget http://pecl.php.net/get/APC-3.1.10.tgz
-tar -xzf APC-3.1.10.tgz
-sh -c "cd APC-3.1.10 && phpize && ./configure && sudo make install"
-echo "extension=apc.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
