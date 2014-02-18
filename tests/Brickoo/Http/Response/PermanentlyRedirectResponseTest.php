e4b681f67bbff55dbf2815730fcadc18f7c244df
Celestino Diaz <celestino.diaz@gmx.de>
2014-02-18 21:40:06 +0100

    added http common responses

---
+++ tests/Brickoo/Http/Response/PermanentlyRedirectResponseTest.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * Copyright (c) 2011-2014, Celestino Diaz <celestino.diaz@gmx.de>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace Brickoo\Tests\Http\Response;
+
+use Brickoo\Http\Response\PermanentlyRedirectResponse,
+    PHPUnit_Framework_TestCase;
+
+/**
+ * PermanentlyRedirectResponseTest
+ *
+ * Test suite for the PermanentlyRedirectResponse class.
+ * @see Brickoo\Http\Response\PermanentlyRedirectResponse
+ * @author Celestino Diaz <celestino.diaz@gmx.de>
+ */
+
+class PermanentlyRedirectResponseTest extends PHPUnit_Framework_TestCase {
+
+    /** @covers Brickoo\Http\Response\PermanentlyRedirectResponse::__construct */
+    public function testRedirectResponse() {
+        $location = "http://brickoo.com/test";
+        $response = new PermanentlyRedirectResponse($location);
+
+        $expectedResponse = "HTTP/1.1 301 Moved Permanently\r\n";
+        $expectedResponse .= sprintf("Location: %s\r\n\r\n", $location);
+        $this->assertEquals($expectedResponse, $response->toString());
+    }
+
+}
