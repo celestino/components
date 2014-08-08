7b908fe8031b95db5034707e59a851c59971c478
Celestino Diaz <celestino.diaz@gmx.de>
2014-08-09 01:20:36 +0200

    added missing property declaration

--- src/Brickoo/Component/Template/PhpTemplate.php
+++ src/Brickoo/Component/Template/PhpTemplate.php
@@ -41,6 +41,9 @@ use Brickoo\Component\Validation\Argument;
 
 class PhpTemplate implements Template {
 
+    /** @var string */
+    protected $templateFile;
+
     /** @var array */
     protected $templateVars;
 
