54f5acd28a91ffaeacc3fde4de22dabd499f7752
Celestino Diaz <celestino.diaz@gmx.de>
2015-03-02 12:44:32 +0100

    Replace listener collection stubs

--- tests/Messaging/ListenerCollectionTest.php
+++ tests/Messaging/ListenerCollectionTest.php
@@ -25,6 +25,7 @@
 namespace Brickoo\Tests\Component\Messaging\Listener;
 
 use Brickoo\Component\Messaging\ListenerCollection;
+use Brickoo\Component\Messaging\MessageListener;
 use PHPUnit_Framework_TestCase;
 
 /**
@@ -40,48 +41,16 @@ class ListenerCollectionTest extends PHPUnit_Framework_TestCase {
      * @covers Brickoo\Component\Messaging\ListenerCollection::__construct
      * @covers Brickoo\Component\Messaging\ListenerCollection::add
      */
-    public function testAddingMessageListeners() {
-        $messageName = "test.add.listener";
-        $priority = 100;
-
-        $listener = $this->getMessageListenerStub();
-        $listener->expects($this->once())
-                 ->method("getMessageName")
-                 ->will($this->returnValue($messageName));
-        $listener->expects($this->once())
-                 ->method("getPriority")
-                 ->will($this->returnValue(0));
+    public function testCommonListenerRoutines() {
+        $listener = new MessageListener("unit-test", 100, function() {});
 
         $listenerCollection = new ListenerCollection();
-        $this->assertEquals(spl_object_hash($listener), $listenerCollection->add($listener, $priority));
-        $this->assertAttributeContains($listener, "listeners", $listenerCollection);
-        $this->assertAttributeCount(1, "listeners", $listenerCollection);
-        $this->assertAttributeContainsOnly("\\Brickoo\\Component\\Messaging\\ListenerPriorityQueue", "listenerQueues", $listenerCollection);
-        $this->assertAttributeCount(1, "listenerQueues", $listenerCollection);
-    }
-
-    /** @covers Brickoo\Component\Messaging\ListenerCollection::get */
-    public function testGettingAMessageListener() {
-        $messageName = "test.add.listener";
-        $priority = 100;
-
-        $listener = $this->getMessageListenerStub();
-        $listener->expects($this->once())
-                 ->method("getMessageName")
-                 ->will($this->returnValue($messageName));
-
-        $listenerCollection = new ListenerCollection();
-        $listenerUID = $listenerCollection->add($listener, $priority);
+        $this->assertEquals(spl_object_hash($listener), ($listenerUID = $listenerCollection->add($listener)));
         $this->assertSame($listener, $listenerCollection->get($listenerUID));
-    }
-
-    /**
-     * @covers Brickoo\Component\Messaging\ListenerCollection::get
-     * @expectedException \InvalidArgumentException
-     */
-    public function testGetIdentifierThrowsArgumentException() {
-        $listenerCollection = new ListenerCollection();
-        $listenerCollection->get(["wrongType"]);
+        $this->assertFalse($listenerCollection->has("unknownUID"));
+        $this->assertTrue($listenerCollection->has($listenerUID));
+        $this->assertSame($listenerCollection, $listenerCollection->remove($listenerUID));
+        $this->assertFalse($listenerCollection->has($listenerUID));
     }
 
     /**
@@ -89,85 +58,17 @@ class ListenerCollectionTest extends PHPUnit_Framework_TestCase {
      * @covers Brickoo\Component\Messaging\Exception\ListenerNotAvailableException
      * @expectedException \Brickoo\Component\Messaging\Exception\ListenerNotAvailableException
      */
-    public function testGetHasNotListenerThrowsException() {
+    public function testTryingToGetUnknownListenerThrowsException() {
         $listenerCollection = new ListenerCollection();
         $listenerCollection->get("doesNotExistIdentifier");
     }
 
-    /** @covers Brickoo\Component\Messaging\ListenerCollection::has */
-    public function testHasMessageListener() {
-        $messageName = "test.add.listener";
-
-        $listener = $this->getMessageListenerStub();
-        $listener->expects($this->once())
-                 ->method("getMessageName")
-                 ->will($this->returnValue($messageName));
-        $listener->expects($this->once())
-                 ->method("getPriority")
-                 ->will($this->returnValue(0));
-
-        $listenerCollection = new ListenerCollection();
-        $listenerUID = $listenerCollection->add($listener);
-
-        $this->assertFalse($listenerCollection->has("fail"));
-        $this->assertTrue($listenerCollection->has($listenerUID));
-    }
-
-    /**
-     * @covers Brickoo\Component\Messaging\ListenerCollection::has
-     * @expectedException \InvalidArgumentException
-     */
-    public function testHasIdentifierThrowsArgumentException() {
-        $listenerCollection = new ListenerCollection();
-        $listenerCollection->has(["wrongType"]);
-    }
-
-    /**
-     * @covers Brickoo\Component\Messaging\ListenerCollection::remove
-     * @covers Brickoo\Component\Messaging\ListenerCollection::getListenerPriorityQueue
-     */
-    public function testRemovingMessageListener() {
-        $messageName = "test.add.listener";
-
-        $listener = $this->getMessageListenerStub();
-        $listener->expects($this->any())
-                 ->method("getMessageName")
-                 ->will($this->returnValue($messageName));
-        $listener->expects($this->once())
-                 ->method("getPriority")
-                 ->will($this->returnValue(0));
-
-        $listener2 = $this->getMessageListenerStub();
-        $listener2->expects($this->any())
-                  ->method("getMessageName")
-                  ->will($this->returnValue($messageName));
-        $listener2->expects($this->once())
-                  ->method("getPriority")
-                  ->will($this->returnValue(0));
-
-        $listenerCollection = new ListenerCollection();
-        $listenerUID = $listenerCollection->add($listener);
-        $listenerCollection->add($listener2);
-
-        $this->assertSame($listenerCollection, $listenerCollection->remove($listenerUID));
-        $this->assertAttributeContains($listener2, "listeners", $listenerCollection);
-    }
-
-    /**
-     * @covers Brickoo\Component\Messaging\ListenerCollection::remove
-     * @expectedException \InvalidArgumentException
-     */
-    public function testRemoveIdentifierThrowsArgumentException() {
-        $listenerCollection = new ListenerCollection();
-        $listenerCollection->remove(["wrongType"]);
-    }
-
     /**
      * @covers Brickoo\Component\Messaging\ListenerCollection::remove
      * @covers Brickoo\Component\Messaging\Exception\ListenerNotAvailableException
      * @expectedException \Brickoo\Component\Messaging\Exception\ListenerNotAvailableException
      */
-    public function testRemoveListenerNotAvailableThrowsException() {
+    public function testTryingToRemoveUnknownListenerThrowsException() {
         $listenerCollection = new ListenerCollection();
         $listenerCollection->remove("failIdentifier");
     }
@@ -177,39 +78,19 @@ class ListenerCollectionTest extends PHPUnit_Framework_TestCase {
      * @covers Brickoo\Component\Messaging\ListenerCollection::collectMessageListeners
      * @covers Brickoo\Component\Messaging\ListenerCollection::getListenerPriorityQueue
      */
-    public function testGetListenersOrderedByPriority() {
-        $messageName = "test.add.listener";
+    public function testGetMessageListenersOrderedByPriority() {
+        $messageName = "test.listener";
 
-        $listener = $this->getMessageListenerStub();
-        $listener->expects($this->any())
-                 ->method("getMessageName")
-                 ->will($this->returnValue($messageName));
-        $listener->expects($this->once())
-                 ->method("getPriority")
-                 ->will($this->returnValue(0));
-
-        $listener2 = $this->getMessageListenerStub();
-        $listener2->expects($this->any())
-                  ->method("getMessageName")
-                  ->will($this->returnValue($messageName));
-        $listener2->expects($this->once())
-                  ->method("getPriority")
-                  ->will($this->returnValue(0));
+        $listener1 = new MessageListener($messageName, 50, function() {});
+        $listener2 = new MessageListener($messageName, 100, function() {});
 
         $listenerCollection = new ListenerCollection();
-        $listenerCollection->add($listener, 100);
-        $listenerCollection->add($listener2, 500);
+        $listenerCollection->add($listener1);
+        $listenerCollection->add($listener2);
 
-        $this->assertEquals(array($listener2, $listener),$listenerCollection->getListeners($messageName));
-    }
-
-    /**
-     * @covers Brickoo\Component\Messaging\ListenerCollection::getListeners
-     * @expectedException \InvalidArgumentException
-     */
-    public function testGetListenersMessageNameThrowsArgumentException() {
-        $listenerCollection = new ListenerCollection();
-        $listenerCollection->getListeners(["wrongType"]);
+        $listeners = $listenerCollection->getListeners($messageName);
+        $this->assertSame($listener2, $listeners[0]);
+        $this->assertSame($listener1, $listeners[1]);
     }
 
     /**
@@ -224,41 +105,11 @@ class ListenerCollectionTest extends PHPUnit_Framework_TestCase {
 
     /** @covers Brickoo\Component\Messaging\ListenerCollection::hasListeners */
     public function testHasListeners() {
-        $messageName = "test.add.listener";
-        $priority = 100;
-
-        $listener = $this->getMessageListenerStub();
-        $listener->expects($this->any())
-                 ->method("getMessageName")
-                 ->will($this->returnValue($messageName));
-        $listener->expects($this->once())
-                 ->method("getPriority")
-                 ->will($this->returnValue(0));
-
+        $messageName = "test.listener";
         $listenerCollection = new ListenerCollection();
-
         $this->assertFalse($listenerCollection->hasListeners($messageName));
-        $listenerCollection->add($listener, $priority);
+        $listenerCollection->add(new MessageListener($messageName, 0, function() {}));
         $this->assertTrue($listenerCollection->hasListeners($messageName));
-    }
-
-    /**
-     * @covers Brickoo\Component\Messaging\ListenerCollection::hasListeners
-     * @expectedException \InvalidArgumentException
-     */
-    public function testHasListenerThrowsMessageNameArgumentException() {
-        $listenerCollection = new ListenerCollection();
-        $listenerCollection->hasListeners(["wrongType"]);
-    }
-
-    /**
-     * Returns a message listener stub.
-     * @return \Brickoo\Component\Messaging\MessageListener
-     */
-    private function getMessageListenerStub() {
-        return $this->getMockBuilder("\\Brickoo\\Component\\Messaging\\Listener")
-            ->disableOriginalConstructor()
-            ->getMock();
     }
 
 }
